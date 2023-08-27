03bacb59bec9354190c4ae76ef190889ca841918
Pape Mayel Diagne THIAM <97792012+mayel15@users.noreply.github.com>
2023-08-27 15:06:31 +0200

    feat(navbar): add a navbar with responsive adaptation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarSectionComponent } from './components/navbar-section/navbar-section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarSectionComponent
   ],
   imports: [
     BrowserModule,
